7d7d3823d7bc9d70c73ea98442ac0ce1acc76fd2
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-05-09 15:03:07 +0300

    feat: add lucide-react-native dependency; enhance ChamaDetails screen with new input fields for name, description, and location; implement improved layout and styles for better user experience

--- app/create/chamaDetails.tsx
+++ app/create/chamaDetails.tsx
@@ -4,20 +4,102 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Dimensions,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
+import FormTitleWithToolTip from "../components/FormTitleWithToolTip";
+import colors from "@/constants/Colors";
+import CustomTextInput from "../components/CustomTextInput";
+import LocationSelector from "../components/LocationSelector";
+const { width } = Dimensions.get("window");
 
 const ChamaDetails = () => {
+  const [chamaName, setChamaName] = useState("");
+  const [chamaDescription, setChamaDescription] = useState("");
+  const [location, setLocation] = useState("");
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={() => router.back()}>
-          <Ionicons name="arrow-back" size={24} color="black" />
-        </TouchableOpacity>
-        <Text style={styles.title}>Chama Details</Text>
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name="arrow-back" size={24} color="black" />
+          </TouchableOpacity>
+          <Text style={styles.title}>Chama Details</Text>
+        </View>
+        <View style={styles.indicatorContainer}>
+          <View
+            style={[styles.indicator, { backgroundColor: colors.chamaBlack }]}
+          ></View>
+          <View style={styles.indicator}></View>
+          <View style={styles.indicator}></View>
+          <View style={styles.indicator}></View>
+        </View>
+        <KeyboardAvoidingView
+          style={styles.formContainer}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <View style={styles.formItemContainer}>
+            <FormTitleWithToolTip
+              title="Profile Image"
+              subtitle="The profile image associated with you chama"
+              tooltipText="A profile image helps other members easily identify you chama. Choose a picture you took together or any other image that is familiar to you and your chama members."
+            />
+            <View style={styles.profileImageContainer}>
+              <TouchableOpacity>
+                <Ionicons name="image-outline" size={44} color="black" />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.formItemContainer}>
+            <FormTitleWithToolTip
+              title="Chama Name"
+              subtitle="The name of your chama"
+              tooltipText="The name of your chama will be used to identify your chama in the app. Other members will also use this name to search it when trying to join the chama."
+            />
+            <TextInput
+              placeholder="Enter your chama name"
+              value={chamaName}
+              onChangeText={setChamaName}
+              style={styles.inputField}
+            />
+          </View>
+          <View style={styles.formItemContainer}>
+            <FormTitleWithToolTip
+              title="Description"
+              subtitle="What is your chama about?"
+              tooltipText="Briefly describe your chama and its purpose. This will help other members understand your chama better. Additionally, this will be used to uniquely categorize your chama in the app."
+            />
+            <TextInput
+              placeholder="Enter your chama description"
+              value={chamaDescription}
+              onChangeText={setChamaDescription}
+              style={[styles.inputField, styles.descriptionInputField]}
+              multiline={true}
+              numberOfLines={4}
+            />
+          </View>
+          <View style={styles.formItemContainer}>
+            <FormTitleWithToolTip
+              title="Location"
+              subtitle="Where is your chama located?"
+              tooltipText="Where are most of your chama members located? Which town or city are you closest to? This will help us when organizing events and meetings."
+            />
+            <LocationSelector value={location} onChange={setLocation} />
+          </View>
+          <TouchableOpacity
+            style={styles.nextButton}
+            onPress={() => router.push("/create/membershipDetails")}
+          >
+            <Text style={styles.nextButtonText}>Next</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -41,5 +123,71 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#212121",
     fontFamily: "MontserratAlternates",
+  },
+  formContainer: {
+    paddingHorizontal: 16,
+  },
+  formItemContainer: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    gap: 10,
+    marginVertical: 16,
+  },
+  profileImageContainer: {
+    borderRadius: width,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.chamaBlack,
+    padding: 10,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    width: width * 0.3,
+    height: width * 0.3,
+    marginTop: 16,
+  },
+  inputField: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.primary,
+    borderRadius: 24,
+    padding: 10,
+    backgroundColor: "#F0F9F7",
+    fontFamily: "JakartaRegular",
+    paddingHorizontal: 16,
+    width: width * 0.9,
+  },
+  descriptionInputField: {
+    height: 100,
+    textAlignVertical: "top",
+    paddingVertical: 14,
+  },
+  indicatorContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    marginVertical: 16,
+  },
+  indicator: {
+    width: width / 5,
+    height: width * 0.01,
+    borderRadius: width,
+    backgroundColor: colors.chamaBlue,
+  },
+  nextButton: {
+    backgroundColor: colors.chamaBlack,
+    borderRadius: 14,
+    padding: 18,
+    marginBottom: 24,
+  },
+  nextButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "JakartSemiBold",
   },
 });
